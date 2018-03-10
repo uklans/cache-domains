5545fb78f722f9e8694c0e18cd47b843209ba853
Mikki Sørensen <Mikki-10@hotmail.com>
2018-03-10 01:41:52 +0100

    Updating index to support cli and web

--- scripts/index.php
+++ scripts/index.php
@@ -1,24 +1,17 @@
 <?php
 
-if (isset($_POST["conf"]) && $_POST["conf"] == "unbound") 
+require_once "lib/include.php";
+
+if (is_cli() === true) 
 {
-	require "unbound_conf.php";
-	$unbound_conf = new unbound_conf();
-	$unbound_conf->print();
+	// Running in cli (commandline)
+	$cli_mode = new cli_mode();
+
 }
 else
 {
-	?>
-
-	<form action="/steamcache/scripts/" method="post">
-		<input type="text" name="ip" placeholder="ip of cache server"><br>
-		<input type="radio" name="conf" value="unbound" checked> unbound<br>
-		<input type="radio" name="conf" value="other"> Other<br>
-		<input type="radio" name="conf" value="other2"> Other2<br>
-		<input type="submit" value="Submit">
-	</form>
-
-	<?php
+	// Running in web interface for hosting
+	$web_mode = new web_mode();
 }
 
 ?>
