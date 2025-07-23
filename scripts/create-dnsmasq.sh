a2a12ae80d7454010e09eff85fa3bfcbef293f60
Amir Zarrinkafsh <nightah@me.com>
2025-07-23 14:09:14 +1000

    feat: add lefthook, megalinter and update editorconfig
    This change adds [LeftHook](https://lefthook.dev/) as a GitHub hook management tool alongside [MegaLinter](https://megalinter.io/) to lint all relevant files within the repository.

--- scripts/create-dnsmasq.sh
+++ scripts/create-dnsmasq.sh
@@ -3,72 +3,73 @@ basedir=".."
 outputdir="output/dnsmasq"
 path="${basedir}/cache_domains.json"
 
-export IFS=' '
+export IFS=" "
 
-test=$(which jq);
-if [ $? -gt 0 ] ; then
-    echo "This script requires jq to be installed."
-    echo "Your package manager should be able to find it"
-    exit 1
+if ! command -v jq >/dev/null; then
+	cat <<-EOF
+		This script requires jq to be installed.
+		Your package manager should be able to find it
+	EOF
+	exit 1
 fi
 
 cachenamedefault="disabled"
 combinedoutput=$(jq -r ".combined_output" config.json)
 
 while read -r line; do
-        ip=$(jq ".ips[\"${line}\"]" config.json)
-        declare "cacheip${line}"="${ip}"
-done <<< $(jq -r '.ips | to_entries[] | .key' config.json)
+	ip=$(jq ".ips[\"${line}\"]" config.json)
+	declare "cacheip${line}"="${ip}"
+done <<<"$(jq -r ".ips | to_entries[] | .key" config.json)"
 
 while read -r line; do
-        name=$(jq -r ".cache_domains[\"${line}\"]" config.json)
-        declare "cachename${line}"="${name}"
-done <<< $(jq -r '.cache_domains | to_entries[] | .key' config.json)
+	name=$(jq -r ".cache_domains[\"${line}\"]" config.json)
+	declare "cachename${line}"="${name}"
+done <<<"$(jq -r ".cache_domains | to_entries[] | .key" config.json)"
 
 rm -rf ${outputdir}
 mkdir -p ${outputdir}
 while read -r entry; do
-    unset cacheip
-    unset cachename
-    key=$(jq -r ".cache_domains[${entry}].name" ${path})
-    cachename="cachename${key}"
-    if [ -z "${!cachename}" ]; then
-        cachename="cachenamedefault"
-    fi
-    if [[ ${!cachename} == "disabled" ]]; then
-        continue;
-    fi
-    cacheipname="cacheip${!cachename}"
-    cacheip=$(jq -r 'if type == "array" then .[] else . end' <<< ${!cacheipname} | xargs)
-    while read -r fileid; do
-        while read -r filename; do
-            destfilename=$(echo ${filename} | sed -e 's/txt/conf/')
-            outputfile=${outputdir}/${destfilename}
-            touch ${outputfile}
-            while read -r fileentry; do
-                # Ignore comments, newlines and wildcards
-                if [[ ${fileentry} == \#* ]] || [[ -z ${fileentry} ]]; then
-                        continue
-                fi
-                parsed=$(echo ${fileentry} | sed -e "s/^\*\.//")
-                for i in ${cacheip}; do
-                    if ! grep -qx "address=/${parsed}/${i}" "${outputfile}"; then
-                        echo "address=/${parsed}/${i}" >> "${outputfile}"
-                    fi
-                    if ! grep -qx "local=/${parsed}/" "${outputfile}"; then
-                        echo "local=/${parsed}/" >> "${outputfile}"
-                    fi
-                done
-            done <<< $(cat ${basedir}/${filename} | sort);
-        done <<< $(jq -r ".cache_domains[${entry}].domain_files[$fileid]" ${path})
-    done <<< $(jq -r ".cache_domains[${entry}].domain_files | to_entries[] | .key" ${path})
-done <<< $(jq -r '.cache_domains | to_entries[] | .key' ${path})
+	unset cacheip
+	unset cachename
+	key=$(jq -r ".cache_domains[${entry}].name" ${path})
+	cachename="cachename${key}"
+	if [ -z "${!cachename}" ]; then
+		cachename=${cachenamedefault}
+	fi
+	if [[ ${cachename} == "disabled" ]]; then
+		continue
+	fi
+	cacheipname="cacheip${!cachename}"
+	cacheip=$(jq -r "if type == \"array\" then .[] else . end" <<<"${!cacheipname}" | xargs)
+	while read -r fileid; do
+		while read -r filename; do
+			destfilename=${filename//txt/conf}
+			outputfile=${outputdir}/${destfilename}
+			touch "${outputfile}"
+			while read -r fileentry; do
+				# Ignore comments, newlines and wildcards
+				if [[ ${fileentry} == \#* ]] || [[ -z ${fileentry} ]]; then
+					continue
+				fi
+				parsed="${fileentry#\*\.}"
+				for i in ${cacheip}; do
+					if ! grep -qx "address=/${parsed}/${i}" "${outputfile}"; then
+						echo "address=/${parsed}/${i}" >>"${outputfile}"
+					fi
+					if ! grep -qx "local=/${parsed}/" "${outputfile}"; then
+						echo "local=/${parsed}/" >>"${outputfile}"
+					fi
+				done
+			done <<<"$(cat ${basedir}/"${filename}" | sort)"
+		done <<<"$(jq -r ".cache_domains[${entry}].domain_files[${fileid}]" ${path})"
+	done <<<"$(jq -r ".cache_domains[${entry}].domain_files | to_entries[] | .key" ${path})"
+done <<<"$(jq -r ".cache_domains | to_entries[] | .key" ${path})"
 
 if [[ ${combinedoutput} == "true" ]]; then
-    for file in ${outputdir}/*; do f=${file//${outputdir}\/} && f=${f//.conf} && echo "# ${f^}" >> ${outputdir}/lancache.conf && cat ${file} >> ${outputdir}/lancache.conf && rm ${file}; done
+	for file in "${outputdir}"/*; do f=${file//${outputdir}\//} && f=${f//.conf/} && echo "# ${f^}" >>${outputdir}/lancache.conf && cat "${file}" >>${outputdir}/lancache.conf && rm "${file}"; done
 fi
 
-cat << EOF
+cat <<EOF
 Configuration generation completed.
 
 Please copy the following files:
--- scripts/create-squid.sh
+++ scripts/create-squid.sh
@@ -4,65 +4,65 @@ outputdir="output/squid"
 path="${basedir}/cache_domains.json"
 REGEX="^\\*\\.(.*)$"
 
-export IFS=' '
+export IFS=" "
 
-test=$(which jq);
-if [ $? -gt 0 ] ; then
-    echo "This script requires jq to be installed."
-    echo "Your package manager should be able to find it"
-    exit 1
+if ! command -v jq >/dev/null; then
+	cat <<-EOF
+		This script requires jq to be installed.
+		Your package manager should be able to find it
+	EOF
+	exit 1
 fi
 
 cachenamedefault="disabled"
 
 while read -r line; do
-    name=$(jq -r ".cache_domains[\"${line}\"]" config.json)
-    declare "cachename${line}"="${name}"
-done <<< $(jq -r '.cache_domains | to_entries[] | .key' config.json)
+	name=$(jq -r ".cache_domains[\"${line}\"]" config.json)
+	declare "cachename${line}"="${name}"
+done <<<"$(jq -r ".cache_domains | to_entries[] | .key" config.json)"
 
 rm -rf ${outputdir}
 mkdir -p ${outputdir}
 while read -r entry; do
-    unset cachename
-    key=$(jq -r ".cache_domains[$entry].name" ${path})
-    cachename="cachename${key}"
-    if [ -z "${!cachename}" ]; then
-        cachename="cachenamedefault"
-    fi
-    if [[ ${!cachename} == "disabled" ]]; then
-        continue;
-    fi
-    while read -r fileid; do
-        while read -r filename; do
-            destfilename=$(echo ${!cachename}.txt)
-            outputfile=${outputdir}/${destfilename}
-            touch ${outputfile}
-            while read -r fileentry; do
-                # Ignore comments
-                if [[ ${fileentry} == \#* ]] || [[ -z ${fileentry} ]]; then
-                    continue
-                fi
-                # Ha wildcards to squid wildcards
-                parsed=$(echo ${fileentry} | sed -e "s/^\*\./\./")
-                # If we have cdn.thing and *.cdn.thing in cache_domains
-                # Squid requires ONLY cdn.thing
-                #
-                # If the fileentry starts with *.cdn.thing
-                if [[ ${fileentry} =~ $REGEX ]]; then
-                    # Does the cache_domains file also contain cdn.thing
-                    grep "${BASH_REMATCH[1]}" ${basedir}/${filename} | grep -v "${fileentry}" > /dev/null
-                    if [[ $? -eq 0 ]]; then
-                        # Skip *.cdn.thing as cdn.thing will be collected earlier/later
-                        continue
-                    fi
-                fi
-                echo "${parsed}" >> "${outputfile}"
-            done <<< $(cat ${basedir}/${filename} | sort);
-        done <<< $(jq -r ".cache_domains[${entry}].domain_files[${fileid}]" ${path})
-    done <<< $(jq -r ".cache_domains[${entry}].domain_files | to_entries[] | .key" ${path})
-done <<< $(jq -r '.cache_domains | to_entries[] | .key' ${path})
+	unset cachename
+	key=$(jq -r ".cache_domains[$entry].name" ${path})
+	cachename="cachename${key}"
+	if [ -z "${!cachename}" ]; then
+		cachename=${cachenamedefault}
+	fi
+	if [[ ${cachename} == "disabled" ]]; then
+		continue
+	fi
+	while read -r fileid; do
+		while read -r filename; do
+			destfilename=${!cachename}.txt
+			outputfile=${outputdir}/${destfilename}
+			touch "${outputfile}"
+			while read -r fileentry; do
+				# Ignore comments
+				if [[ ${fileentry} == \#* ]] || [[ -z ${fileentry} ]]; then
+					continue
+				fi
+				# Ha wildcards to squid wildcards
+				parsed="${fileentry#\*}"
+				# If we have cdn.thing and *.cdn.thing in cache_domains
+				# Squid requires ONLY cdn.thing
+				#
+				# If the fileentry starts with *.cdn.thing
+				if [[ ${fileentry} =~ $REGEX ]]; then
+					# Does the cache_domains file also contain cdn.thing
+					if grep "${BASH_REMATCH[1]}" ${basedir}/"${filename}" | grep -v "${fileentry}" >/dev/null; then
+						# Skip *.cdn.thing as cdn.thing will be collected earlier/later
+						continue
+					fi
+				fi
+				echo "${parsed}" >>"${outputfile}"
+			done <<<"$(cat ${basedir}/"${filename}" | sort)"
+		done <<<"$(jq -r ".cache_domains[${entry}].domain_files[${fileid}]" ${path})"
+	done <<<"$(jq -r ".cache_domains[${entry}].domain_files | to_entries[] | .key" ${path})"
+done <<<"$(jq -r ".cache_domains | to_entries[] | .key" ${path})"
 
-cat << EOF
+cat <<EOF
 Configuration generation completed.
 
 Please copy the following files:
